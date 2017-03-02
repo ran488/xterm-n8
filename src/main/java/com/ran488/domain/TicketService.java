c065adc4eb4d22df035e612af55f49da4c57ce3b
Robb Nicholson <robb.nicholson@gm.com>
2017-03-02 11:22:57 -0700

    add logging to email

--- src/main/java/com/ran488/domain/TicketService.java
+++ src/main/java/com/ran488/domain/TicketService.java
@@ -1,8 +1,12 @@
 package com.ran488.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+
+import com.ran488.ws.RestEndpoint;
 
 /**
  * User-submitted ticket representing a request sent to another group for
@@ -12,6 +16,8 @@ import org.springframework.mail.SimpleMailMessage;
  */
 public class TicketService {
 
+	private static final Logger log = LoggerFactory.getLogger(TicketService.class);
+
 	private final MailSender mailSender;
 	private SimpleMailMessage templateMessage;
 
@@ -20,17 +26,28 @@ public class TicketService {
 		this.mailSender = mailSender;
 		this.templateMessage = templateMessage;
 	}
-	
+
 	/**
 	 * Notify originator of the ticket/request with status of the ticket
 	 * submission.
 	 * 
 	 */
 	public void notifyOriginator(final String emailAddress, final String content) throws MailException {
+		log.info("Sending email to: " + emailAddress);
+		log.info("Content: " + content);
 		SimpleMailMessage msg = new SimpleMailMessage(this.templateMessage);
 		msg.setTo(emailAddress);
 		msg.setText(content);
-		this.mailSender.send(msg);
+		try {
+			this.mailSender.send(msg);
+		} catch (MailException e) {
+			log.error("email send failed", e);
+			throw e;
+		} catch (Exception e) {
+			log.error("email send failed", e);
+			throw e;
+		}
+		log.info("Sent");
 	}
 
 }
